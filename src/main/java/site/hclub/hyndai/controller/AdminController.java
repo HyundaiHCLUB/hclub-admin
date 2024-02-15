7905ea6e05f4d891dd6fe7a44250e419964fda9e
codeSweet0828 <codesweet0828@gmail.com>
2024-02-15 14:06:54 +0900

    동아리 관리 Admin Backend-Api

---
+++ src/main/java/site/hclub/hyndai/controller/AdminController.java
@@ -0,0 +1,60 @@
+package site.hclub.hyndai.controller;
+import static site.hclub.hyndai.common.response.SuccessType.*;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j;
+import site.hclub.hyndai.common.response.ApiResponse;
+import site.hclub.hyndai.domain.ClubVO;
+import site.hclub.hyndai.dto.ClubDTO;
+import site.hclub.hyndai.service.ClubService;
+
+/**
+ * @author 김은솔
+ * @description: 관리자 관련 Contoller를 작성한다.
+ * ===========================
+	   AUTHOR      NOTE
+ * ---------------------------
+ *    김은솔         최초생성
+ * ===========================
+ */
+@RestController
+@RequestMapping("/admin")
+@Log4j
+public class AdminController {
+
+	@Autowired
+	private ClubService clubService;
+	
+	 @GetMapping("/club")
+	 public ResponseEntity<ApiResponse<List<ClubDTO>>> getClubList(@RequestBody ClubVO cvo) {
+		 
+		 return ApiResponse.success(GET_CLUB_LIST_SUCCESS, clubService.getClubList(cvo));
+	 }
+	 @GetMapping("/clubCnt")
+	 public ResponseEntity<ApiResponse<Integer>> getClubCnt(@RequestBody ClubVO cvo) {
+		 
+		 return ApiResponse.success(GET_CLUB_LIST_CNT_SUCCESS, clubService.getClubCnt(cvo));
+	 }
+	 @DeleteMapping("/club/{clubNo}")
+	 public ResponseEntity<ApiResponse<Integer>> deleteClubInfo(@PathVariable String clubNo) {
+		 
+		 return ApiResponse.success(DELETE_CLUB_INFO_SUCCESS, clubService.deleteClubInfo(clubNo));
+	 }
+	 @GetMapping("/club/updateUseYn/{clubNo}")
+	 public ResponseEntity<ApiResponse<Integer>> updateClubUseYn(@PathVariable String clubNo) {
+		 
+		 return ApiResponse.success(UPDATE_CLUB_USE_YN_SUCCESS, clubService.updateClubUseYn(clubNo));
+	 }
+	 
+}
---
+++ src/main/java/site/hclub/hyndai/service/ClubServiceImpl.java
@@ -0,0 +1,38 @@
+package site.hclub.hyndai.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import site.hclub.hyndai.domain.ClubVO;
+import site.hclub.hyndai.dto.ClubDTO;
+import site.hclub.hyndai.mapper.ClubMapper;
+
+@Service
+public class ClubServiceImpl implements ClubService {
+
+	@Autowired
+	private ClubMapper clubMapper;
+	
+	@Override
+	public List<ClubDTO> getClubList(ClubVO cvo) {
+	
+		return clubMapper.getClubList(cvo);
+	}
+	@Override
+	public int getClubCnt(ClubVO cvo) {
+		
+		return clubMapper.getClubCnt(cvo);
+	}
+	@Override
+	public int deleteClubInfo(String clubNo) {
+		return clubMapper.deleteClubInfo(clubNo);
+	}
+	@Override
+	public int updateClubUseYn(String clubNo) {
+		
+		return clubMapper.updateClubUseYn(clubNo);
+	}
+
+}
